26656a2f7efce0fbd2bea78d22b655016b4b181c
Nick Ganter <nickganter91@gmail.com>
2017-06-11 21:59:15 -0500

    Added new tables Added new entities

---
+++ LeafGreen.Entities/Garden.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeafGreen.Entities
+{
+    public class Garden
+    {
+        public int GardenId { get; set; }
+        public string GardenName { get; set; }
+        public bool IsArchived { get; set; }
+        public DateTime DateAdded { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
---
+++ LeafGreenDb/plants/Tables/Gardens.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [plants].[Gardens] (
+    [GardenId]   INT               IDENTITY (1, 1) NOT NULL,
+    [GardenName] VARCHAR (250)     NOT NULL,
+    [IsArchived] BIT               NOT NULL,
+    [DateAdded]  DATETIME2 (7)     NOT NULL,
+    [Location]   [sys].[geography] NOT NULL,
+    CONSTRAINT [PK_Gardens] PRIMARY KEY CLUSTERED ([GardenId] ASC)
+);
+
