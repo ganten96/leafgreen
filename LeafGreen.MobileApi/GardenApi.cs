5373cfb9162ce9f69295a814a535e9b9ee238bee
Nick Ganter <nickganter91@gmail.com>
2017-07-24 18:57:51 -0500

    Updated sql project. Added api project for mobile

---
+++ LeafGreen.MobileApi/GardenApi.cs
@@ -0,0 +1,20 @@
+﻿using LeafGreen.Entities;
+using System;
+
+namespace LeafGreen.MobileApi
+{
+    public class GardenApi
+    {
+        private readonly string _deviceId;
+
+        public GardenApi(string deviceId)
+        {
+            _deviceId = deviceId;
+        }
+
+        //public async Garden GetGardenByGardenId(int gardenId)
+        //{
+        //    return await null;
+        //}
+    }
+}
